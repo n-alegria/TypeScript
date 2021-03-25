ec1a4abc7a2f34efbc302e6a47ffd71a97f0bcec
Lautaro <n.alegria@outlook.com>
2021-03-24 21:54:38 -0300

    Clase 01

--- Clase 1/Ejercicios/Ejercicio11/app.ts
+++ Clase 1/Ejercicios/Ejercicio11/app.ts
@@ -5,32 +5,6 @@ desde la izquierda y desde la derecha.
 Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural".
 */
 
-
-// function Palíndromo(oracion: string): void {
-//     oracion = oracion.toLocaleLowerCase();
-//     oracion = oracion.replace(/\s/g, "");
-//     var bandera: boolean = false;
-//     var i: number = 0;
-//     for (var j: number = (oracion.length - 1); j >= (oracion.length / 2); j--) {
-//         //console.log(oracion.charAt(i) + oracion.charAt(j));
-//         if (oracion.charAt(i) == oracion.charAt(j)) {
-
-//         }
-//         else {
-//             bandera = true;
-//             break;
-//         }
-//         i++;
-//     }
-//     if (bandera) {
-//         console.log("Es un palíndromo");
-//     }
-//     else {
-//         console.log("NO es un palíndromo");
-//     }
-
-// }
-
 let palindromo = (cadena: string) :void =>{
     cadena = cadena.toLowerCase().replace(/\s/g, "");
     let cadenaInversa: string = cadena.split("").reverse().join("");
